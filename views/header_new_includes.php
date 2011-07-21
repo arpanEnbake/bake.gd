50b0a6033ae6cc2af65d85175d94145b2d839e3f
ishwinder@enbake.com <Ishwinder>
2011-07-21 04:47:31 -0500

    misssed file : ishu ui chagnes

---
+++ views/header_new_includes.php
@@ -0,0 +1,27 @@
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>bake.gd | Basic | Every link builds a brand</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta content="IE=8" http-equiv="X-UA-Compatible" />
+<meta name="keywords" content="bake.gd, brand building, link marketting" />
+<meta name="description" content="bake.gd, build your brand with every short link" />
+<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+
+<script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
+
+<base href="<?php echo base_url(); ?>" />
+<link rel="shortcut icon" href="<?php echo base_url(); ?>favicon.ico" />
+
+<!-- Stylesheets-->
+<link href="resource/app/css/bake.css" rel="stylesheet"  type="text/css">
+<link href="resource/app/css/jquery-ui-1.8.13.custom.css" rel="stylesheet"  type="text/css">
+
+<script type="text/javascript" src="resource/app/js/jquery-1.5.1.min.js"></script>
+<script type="text/javascript" src="resource/app/js/jquery-ui-1.8.13.custom.min.js"></script>
+
+<!--[if lte IE 8]>
+<link rel="stylesheet" href="IE.css" type="text/css" charset="utf-8" />
+<![endif]-->
+
+<!-- scripts-->
+</head>
