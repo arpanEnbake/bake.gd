31611695a379eacd9f1e4f38ee2bffd9d8e30692
Ishu <ishwinder@enbake.com>
2011-07-28 18:25:44 +0530

    Fixes to detail view page !!
    Signed-off-by: Ishu <ishwinder@enbake.com>

--- views/detail.php
+++ views/detail.php
@@ -1,40 +1,56 @@
-<?php echo $this->load->view('inner_elements/header'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php echo $this->load->view('header_new_includes');?>
+<body>
+	<?php echo $this->load->view('inner_elements/header_new'); ?>
+	
+	<?php $host = isset($account) && isset($account->domain) ? $account->domain : $_SERVER['HTTP_HOST'];?>
+	<?php if(!isset($twitter)) $twitter = null;?>
+	<?php if(!isset($fb)) $fb = null;?>
 
-<?php $host = isset($account) && isset($account->domain) ? $account->domain : $_SERVER['HTTP_HOST'];?>
-<?php if(!isset($twitter)) $twitter = null;?>
-<?php if(!isset($fb)) $fb = null;?>
-
-
-<!-- BODY START-->
-<div id="mainBody">
-<div id="body">
-<div class="content">
-<div class="left">
-			
-</div>  <!-- end left -->
-</div>
-
-			<h1>Shortened URL: <?php echo 'http://' . $url->domain . '/' . $url->keyword;?></h1>
-			Created on <?php echo $url->timestamp?> by <?php echo $user->username?>
-			<h2>Keyword: <?php echo $url->keyword?></h2>
-			<h2>URL: <?php echo $url->url?></h2>
-			<h3>Clicks:</h3>
-			<?php $total_count = 0;
-				if (isset($clicks)) {
-				foreach($clicks as $key => $count) {
-					echo "Clicks via  $count->referrer : $count->Count <br />";
-					$total_count += $count->Count;
+	<!-- BODY START-->
+	<div id="mainBody">
+		<div id="body">
+			<div class="content">
+				<div class="subheader">
+					<span style="float:left">
+						<h1><?php echo 'http://' . $url->domain . '/' . $url->keyword;?></h1>
+					</span>
+					<span style="float:right;">
+						<ul>
+							<li><a href="#">Copy</a></li>
+							<li><a href="#">Share</a></li>
+						</ul>
+					</span>
+				</div>
+				<?php $total_count = 0;
+					if (isset($clicks)) {
+					foreach($clicks as $key => $count) {
+						echo "Clicks via  $count->referrer : $count->Count <br />";
+						$total_count += $count->Count;
+					}
 				}
-			}
-			echo "Total Clicks: $total_count";
-			?>
-			
-<?php echo "<h2>Retweets made : $url->retweets </h2><br />" ;?>
-<?php echo "<h2>FB Likes : $url->likes </h2><br />" ;?>
-
-
-
-<?php echo  $this->load->view('analytics/graphs'); ?>
-</div> <!-- body -->
-</div> <!-- main body -->
-<?php echo $this->load->view('inner_elements/footer'); ?>
+				?>
+				<div class="link_detail">
+					<h2 id="link_title"><?php echo $url->title; ?></h2>
+					<ul class="stats">
+						<li class="clearfix">
+							<span class="label">Link:</span><?php echo $url->url; ?>
+						</li>
+						<li class="clearfix">
+							<span class="label"  style="line-height:24px;">Stats:</span>
+							<ul style="line-height:24px;">
+								<li style="display:inline;"><span class="count"><?php echo $total_count; ?></span>Clicks</li>
+								<li style="display:inline;"><span class="count"><?php echo $url->retweets > 0 ? $url->retweets : 0; ?></span>RTs</li>
+								<li style="display:inline;"><span class="count"><?php echo $url->likes > 0 ? $url->likes : 0; ?></span>Likes</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+				<?php echo  $this->load->view('analytics/graphs'); ?>
+			</div> <!-- body -->
+		</div> <!-- main body -->
+	</div>
+	<?php echo $this->load->view('inner_elements/footer'); ?>
+</body>
+</html>
